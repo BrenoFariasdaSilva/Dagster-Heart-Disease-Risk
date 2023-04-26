715772a0528aad52e466d222da40cd3f444b712a
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-26 10:05:44 -0300

    Sarting from scratch this project in Dagster

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     packages=find_packages(exclude=["heart_disease_risk_tests"]),
     install_requires=[
         "dagster",
-        "dagster-cloud"
     ],
     extras_require={"dev": ["dagit", "pytest"]},
 )
