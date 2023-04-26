db859bc1c3755bb584989708d5405d8ae82ff151
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-26 10:38:17 -0300

    Creating asset to read csv file and store it into panda dataframe

--- heart_disease_risk/assets.py
+++ heart_disease_risk/assets.py
@@ -1,1 +1,12 @@
+# Assets are used to create data pipelines
+# Data pipelines are used to read data, process it and return it in a format that can be used by the model
 
+from dagster import asset # Import the asset decorator
+import pandas as pd # Import the pandas library
+
+# Function that reads the data from the csv file and stores it in a pandas dataframe
+## Panda dataframe is a 2-dimensional labeled data structure with columns of potentially different types, similar to an Excel spreadsheet or SQL table
+@asset # Decorator that marks the function as an asset
+def read_csv_data(): 
+	dataframe = pd.read_csv('../data/risco_cardiaco.csv')
+	return dataframe
