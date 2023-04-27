5d5c23d117f2a1dca2d6a5a057307374a3ea9081
BrenoFariasdaSilva <breno.vicio78@gmail.com>
2023-04-27 08:21:49 -0300

    Moving assets.py from heart_disease_risk/  to heart_disease_risk/assets/

--- heart_disease_risk/__init__.py
+++ heart_disease_risk/__init__.py
@@ -1,6 +1,13 @@
 from dagster import Definitions, load_assets_from_modules
 
-from . import assets
+# If you move the assets.py file to a different directory than the project root, you will need to update the import statement below.
+# I moved the assets.py file from the heart_disease_risk/ to the heart_disease_risk/assets directory
+# With that in mind, i renamed the assets.py file to __init__.py
+# It allows me to import the assets from the assets directory without having to specify the file name 
+# So the import went from "from assets.assets import assets" to "from assets import assets"
+# It makes the code more readable and easier to maintain
+
+from assets import assets
 
 all_assets = load_assets_from_modules([assets])
 
